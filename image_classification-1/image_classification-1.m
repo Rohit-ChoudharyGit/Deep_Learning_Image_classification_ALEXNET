a18efdeab6b9037ff5e3dbba4239fc4954674727
Rohit Choudhary <41263406+Rohit-ChoudharyGit@users.noreply.github.com>
2019-01-05 03:36:19 +0530

    Update image_classification-1.m

--- image_classification-1/image_classification-1.m
+++ image_classification-1/image_classification-1.m
@@ -1,18 +1,18 @@
-
+%%just like the library ,import alexnet into a variable deepnet
 deepnet = alexnet;
-
-img1 = imread('10.png');
+%get some image files
+img1 = imread('10.png'); 
 img2 = imread('58.png');
 img3 = imread('49.png');
 
-
+% show them out
 figure;
 imshow(img1)
 figure;
 imshow(img2)
 figure;
 imshow(img3)
-
+%%resize the images to the standard input size of alexnet input layer
 img1 = imresize(img1,[227 227]);
 figure;
 imshow(img1)
@@ -26,7 +26,9 @@ figure;
 imshow(img3)
 
 
-
+%% finally what the alexnet predicts
 pd1 = classify(deepnet, img1)
 pd2 = classify(deepnet,img2)
 pd3 = classify(deepnet,img3)
+
+%%in future files ,will look for more methods 
